8667e5d69dc6079d27ab96f6d0cf5069bfae7773
RayBB <RayBB@users.noreply.github.com>
2025-11-24 16:56:17 -0800

    refactor: Remove React integration and its associated dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
+
 import tailwindcss from "@tailwindcss/vite";
 
 import { template } from "./src/settings";
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), sitemap()],
+    integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()],
     },
